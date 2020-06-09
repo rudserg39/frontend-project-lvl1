50fc1a1a6cc6dd9ea0fe6d6de446c4b7bec793ed
Sergei Rudkovskii <rudserg39@gmail.com>
2020-06-09 19:19:43 +0200

    Make corrections

--- games/gcd.js
+++ games/gcd.js
@@ -4,7 +4,9 @@ import gameRunner from '../src/index.js';
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
-const findGreatestCommonDivisor = (num1, num2) => (num2 === 0 ? num1 : findGreatestCommonDivisor(num2, num1 % num2));
+const findGreatestCommonDivisor = (num1, num2) => (
+  num2 === 0 ? num1 : findGreatestCommonDivisor(num2, num1 % num2)
+);
 
 const prepareGameData = () => {
   const num1 = getRandomInteger(1, 15);
